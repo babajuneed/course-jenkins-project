d2a86dd13a93e7e377cd094092ca2f1790c59b22
babajuneed <babajuneed@gmail.com>
2025-07-26 18:45:19 +0530

    test

--- 03-lambda-deployment/lambda-app/hello_world/app.py
+++ 03-lambda-deployment/lambda-app/hello_world/app.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world v3",
+            "message": "hello world v1",
             # "location": ip.text.replace("\n", "")
         }),
     }
